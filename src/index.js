3bdc9e1e1ab3692bb8f9676a393f0732f6096ca8
Embleslyies <embleslyiesroblox@gmail.com>
2026-09-26 02:22:37 +0800

    does it launch in order???

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const myElement = document.getElementById("coolText")
 var i = 0;
 while (true) {
     i++;
+    console.log(myElement)
     myElement.textContent = i;
 
     new Promise(resolve => setTimeout(resolve, 1000))
